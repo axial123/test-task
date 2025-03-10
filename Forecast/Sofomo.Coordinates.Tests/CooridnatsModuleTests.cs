a5659d95dbf360532a6bbfd0cbe995b928febed2
Denis Spitsyn <denys.spitsyn@xebia.com>
2025-03-10 17:53:02 +0100

    add validation for api, update naming for unit test

--- Forecast/Sofomo.Coordinates.Tests/CooridnatsModuleTests.cs
+++ Forecast/Sofomo.Coordinates.Tests/CooridnatsModuleTests.cs
@@ -8,10 +8,13 @@ namespace Sofomo.Coordinates.Tests;
 public class CooridnatsModuleTests(Fixture fixture): TestBase<Fixture>
 {
     [Fact]
-    public async Task GetAll_ShouldReturn_AllCoordinates()
+    public async Task GetAll_ShouldReturnAllCoordinates()
     {
+        // arrange 
+        // act
         var result = await fixture.Client.GETAsync<GetAll, GetAllCoordinatesResponse>();
 
+        // assert
         result.Response.EnsureSuccessStatusCode();
         result.Result.Coordinates.ToList().Count.Should().Be(3);
     }
---
+++ Forecast/Sofomo.Coordinates/CoordinatesEndpoints/Create.CreateCoordinatesRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace Sofomo.Coordinates.Endpoints;
+
+internal class CreateCoordinatesRequestValidator : Validator<CreateCoordinatesRequest>
+{
+    public CreateCoordinatesRequestValidator()
+    {
+        RuleFor(x => x.Latitude).GreaterThan(0).WithMessage("Latitude cannot be negative");
+        RuleFor(x => x.Longitude).GreaterThan(0).WithMessage("Longitude cannot be negative");
+    }
+}
